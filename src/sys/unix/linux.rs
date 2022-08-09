70e4a9f72812e70238fa84f4f4494756e01673d6
Jiahao XU <Jiahao_XU@outlook.com>
2022-08-09 19:07:59 +1000

    Revert use of `NamedTempFile` in `unix/linux.rs`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/sys/unix/linux.rs
+++ src/sys/unix/linux.rs
@@ -2,7 +2,6 @@ use std::{convert::TryInto, fs, io, mem::size_of, os::unix::io::AsRawFd, path::P
 
 use ioctl_sys::iow;
 use libc::c_int;
-use tempfile::NamedTempFile;
 
 const C_INT_SIZE: usize = size_of::<c_int>();
 const FICLONE: u32 = iow!(0x94, 9, C_INT_SIZE);
@@ -11,11 +10,10 @@ pub fn reflink(from: &Path, to: &Path) -> io::Result<()> {
     let src = fs::File::open(&from)?;
 
     // pass O_EXCL to mimic macos behaviour
-    let to_parent = to
-        .parent()
-        .ok_or_else(|| io::Error::from(io::ErrorKind::InvalidInput))?;
-    let dest = NamedTempFile::new_in(to_parent)?;
-
+    let dest = fs::OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(&to)?;
     let ret = unsafe {
         // http://man7.org/linux/man-pages/man2/ioctl_ficlonerange.2.html
         libc::ioctl(
@@ -26,10 +24,11 @@ pub fn reflink(from: &Path, to: &Path) -> io::Result<()> {
     };
 
     if ret == -1 {
-        Err(io::Error::last_os_error())
+        let err = io::Error::last_os_error();
+        // remove the empty file that was created.
+        let _ = fs::remove_file(to);
+        Err(err)
     } else {
-        dest.persist_noclobber(to)
-            .map_err(|persist_err| persist_err.error)?;
         Ok(())
     }
 }

5842f452ac98041efdae98f0796d47b39597c389
Jiahao XU <Jiahao_XU@outlook.com>
2022-08-10 00:25:17 +1000

    Update src/sys/utility.rs
    Co-authored-by: Félix Saparelli <felix@passcod.name>

--- src/sys/utility.rs
+++ src/sys/utility.rs
@@ -1,37 +1,44 @@
-#![allow(dead_code)]
-
 use std::{
     fs::{remove_file, File},
     io,
     path::{Path, PathBuf},
 };
 
-use derive_destructure2::destructure;
+#[cfg(unix)]
+use std::os::unix::io::{RawFd, AsRawFd};
 
-#[derive(Debug, destructure)]
+#[derive(Debug)]
 pub(super) struct AutoRemovedFile {
-    pub(super) inner: File,
+    // Option<File> uses File's niche, so this is zero cost
+    inner: Option<File>,
     path: PathBuf,
 }
 
 impl AutoRemovedFile {
-    pub(super) fn create_new(path: &Path) -> io::Result<Self> {
+    pub fn create_new(path: &Path) -> io::Result<Self> {
         // pass O_EXCL to mimic macos behaviour
         let inner = File::options().write(true).create_new(true).open(path)?;
 
         Ok(Self {
-            inner,
+            inner: Some(inner),
             path: path.into(),
         })
     }
 
-    pub(super) fn persist(self) {
-        self.destructure();
+    #[cfg(unix)]
+    pub fn as_raw_fd(&self) -> Option<RawFd> {
+        self.inner.as_ref().map(|file| file.as_raw_fd())
+    }
+
+    pub fn persist(mut self) {
+        self.inner.take();
     }
 }
 
 impl Drop for AutoRemovedFile {
     fn drop(&mut self) {
-        let _ = remove_file(&self.path);
+        if self.inner.is_some() {
+            let _ = remove_file(&self.path);
+        }
     }
 }

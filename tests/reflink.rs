4eecf8ae5c82b437fa071f46623048e3bda0c4ca
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-10-14 12:24:08 +0200

    test: remove unused test

--- tests/reflink.rs
+++ tests/reflink.rs
@@ -1,7 +1,6 @@
 use std::fs::{self, File};
 use std::io;
 use std::path::Path;
-use std::env;
 use tempfile::tempdir;
 
 use reflink_copy::{reflink, reflink_or_copy};
@@ -116,43 +115,4 @@ fn reflink_or_copy_ok() {
         input.metadata().unwrap().permissions(),
         out.metadata().unwrap().permissions()
     );
-}
-
-#[test]
-fn clone_file_ten_thousand_times() {
-    // Create a tmp directory in the current working directory
-    let current_dir = env::current_dir().unwrap();
-    let tmp_dir = current_dir.join("tmp_test_dir");
-    fs::create_dir_all(&tmp_dir).unwrap();
-
-    let src_file_path = tmp_dir.join("src.txt");
-
-    // Write some content to the source file
-    fs::write(&src_file_path, b"this is a test").unwrap();
-
-    let mut errors = 0;
-
-    // Loop to create 10,000 clones
-    for i in 0..10_000 {
-        let dest_file_path = tmp_dir.join(format!("dest_{}.txt", i));
-        match reflink(&src_file_path, &dest_file_path) {
-            Ok(()) => {
-                // Check if the cloned file has the same content as the source
-                if fs::read(&dest_file_path).unwrap() != b"this is a test" {
-                    println!("Clone {} does not match source file!", i);
-                    errors += 1;
-                }
-            }
-            Err(e) => {
-                println!("Failed to clone file on iteration {}: {:?}", i, e);
-                errors += 1;
-            }
-        }
-    }
-
-    // Clean up the temporary directory after the test
-    fs::remove_dir_all(&tmp_dir).unwrap();
-
-    // Assert that there were no errors during the 10,000 cloning operations
-    assert_eq!(errors, 0);
 }

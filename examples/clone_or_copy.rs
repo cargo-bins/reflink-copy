4cdcf9012f0b89dfad948ba6e4fc65c3360d1893
Vaiz <4908982+Vaiz@users.noreply.github.com>
2024-11-03 11:13:08 +0100

    add clone_or_copy example

---
+++ examples/clone_or_copy.rs
@@ -0,0 +1,14 @@
+// cargo run --example clone_or_copy V:\file.bin V:\file-clone.bin
+
+fn main() -> std::io::Result<()>{
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: {} <source_file> <target_file>", args[0]);
+        return Ok(());
+    }
+    let src_file = &args[1];
+    let tgt_file = &args[2];
+
+    let _ = reflink_copy::reflink_or_copy(src_file, tgt_file)?;
+    Ok(())
+}
